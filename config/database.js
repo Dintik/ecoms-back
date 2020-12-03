01d38ed6131eba9dbb2a88c1e961afc6d2f06c6a
Artem Chaylo <achaylo@visiblemagic.com>
2020-12-03 16:19:48 +0200

    upd

--- config/database.js
+++ config/database.js
@@ -13,19 +13,3 @@ module.exports = () => ({
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', 'db/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
